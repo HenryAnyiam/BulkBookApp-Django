63e9a45b4d94f5f32ada345359c75daa36ef45ab
Henry Anyiam <anyiamhenry7@gmail.com>
2025-09-04 06:17:11 +0100

    Add app and starter model

---
+++ main/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Category(models.Model):
+
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(null=False, max_length=250)
+    display_order = models.IntegerField()
+    date_created = models.DateTimeField(default=timezone.now)
