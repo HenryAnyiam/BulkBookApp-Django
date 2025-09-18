b139983807a5d0fb9642b12c2c1bc13ce6283677
Henry Anyiam <anyiamhenry7@gmail.com>
2025-09-18 05:10:59 +0100

    Add migrations

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 # Create your models here.
 class Category(models.Model):
 
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(null=False, max_length=250)
     display_order = models.IntegerField()
     date_created = models.DateTimeField(default=timezone.now)
--- main/views.py
+++ main/views.py
@@ -4,4 +4,4 @@ from main.models import Category
 # Create your views here.
 def index(request):
     category_obj = Category.objects.all()
-    return render(request, "index.html", context={'category': category_obj})
+    return render(request, "index.html", context={'categories': category_obj})
